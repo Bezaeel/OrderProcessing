91202c39c7fe25684166676e6669bce844cea73e
Bezaeel <talabi.rotimi@hotmail.com>
2019-11-02 15:42:43 +0100

    Update and Delete category feature implementation

--- API/Domain/Services/Communication/CategoryResponse.cs
+++ API/Domain/Services/Communication/CategoryResponse.cs
@@ -9,16 +9,16 @@ namespace API.Domain.Services.Communication
     /// <summary>
     /// Refactor to generic type
     /// </summary>
-    public class SaveCategoryResponse : BaseResponse
+    public class CategoryResponse : BaseResponse
     {
         public Category Category { get; private set; }
-        private SaveCategoryResponse(bool success, string message, Category category) : base(success, message)
+        private CategoryResponse(bool success, string message, Category category) : base(success, message)
         {
             Category = category;
         }
-        public SaveCategoryResponse(Category category) : this(true, string.Empty, category)
+        public CategoryResponse(Category category) : this(true, string.Empty, category)
         { }
-        public SaveCategoryResponse(string message) : this(false, message, null)
+        public CategoryResponse(string message) : this(false, message, null)
         { }
     }
 }
--- API/Persistence/Repositories/CategoryRepository.cs
+++ API/Persistence/Repositories/CategoryRepository.cs
@@ -15,15 +15,30 @@ namespace API.Persistence.Repositories
         {
         }
 
-        public async Task AddAsync(Category category)
-        {
-            await _context.Categories.AddAsync(category);
-        }
 
         public async Task<IEnumerable<Category>> ListAsync()
         {
             return await _context.Categories.ToListAsync();
         }
 
+        public async Task AddAsync(Category category)
+        {
+            await _context.Categories.AddAsync(category);
+        }
+
+        public async Task<Category> FindByIdAsync(int id)
+        {
+            return await _context.Categories.FindAsync(id);
+        }
+
+        public void Update(Category category)
+        {
+            _context.Categories.Update(category);
+        }
+
+        public void Remove(Category category)
+        {
+            _context.Categories.Remove(category);
+        }
     }
 }
